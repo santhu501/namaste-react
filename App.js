7ea2cd91c971bc78d7ffee64a0044933d71a158b
santhu501 <santhosh.mittapally@yahoo.com>
2023-05-08 17:02:07 -0400

    Functional components example.

--- App.js
+++ App.js
@@ -23,6 +23,7 @@ console.log(parentElement); */
 
 // JSX is converted to react element by Babel. JSX is not html.
 // JSX can be used with parenthesis for multiple lines, for single line paranthesis is optional.
+// React Element is a JS object that is later converted to HTML as part of render.
 const JSXElement = <h1 className="parent"> namaste react </h1>;
 
 const JSX2 = (<h1 className="parent" tabIndex="5"> 
@@ -33,3 +34,21 @@ console.log(JSXElement);
 //const heading = React.createElement("h1", {id:"h1Element"}, "first react");
 const rootElem = ReactDOM.createRoot(document.getElementById("root"));
 rootElem.render(JSXElement);
+
+// React functional components are the ones that are mostly used now.
+// React class based components usage is pretty old pattern.
+// A function that returns React element or JSX is a React functional component.
+
+const headingComponent = () => {
+    return <h1 classNa="parent"> Namaste React </h1>
+};
+
+// or for one line statements, we can right like below ways.
+
+const headingComponent2 = () => <h1 className="parent"> Namaste React </h1>;
+
+// or 
+
+const headingComponent3 = () => (
+<h1 className="parent"> Namaste React </h1>
+);
