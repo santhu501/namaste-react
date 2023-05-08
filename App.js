acda6a5a5323d84e7337d37b6933f04037aae3a8
santhu501 <santhosh.mittapally@yahoo.com>
2023-05-08 16:31:59 -0400

    few more changes. using JSX.

--- App.js
+++ App.js
@@ -14,12 +14,22 @@ import ReactDOM from 'react-dom'
  * 
  * 
  */
-
+/* 
 const parentElement = React.createElement("div", { id: "parent" },
         React.createElement("div", { id: "child" },
             [ React.createElement("h1", {key: "1"}, "this is h1 tag"), React.createElement("h2", {key: "2"}, "this is h2 tag") ] ));
 
-console.log(parentElement);
+console.log(parentElement); */
+
+// JSX is converted to react element by Babel. JSX is not html.
+// JSX can be used with parenthesis for multiple lines, for single line paranthesis is optional.
+const JSXElement = <h1 className="parent"> namaste react </h1>;
+
+const JSX2 = (<h1 className="parent" tabIndex="5"> 
+namaste react </h1> );
+
+console.log(JSXElement);
+
 //const heading = React.createElement("h1", {id:"h1Element"}, "first react");
 const rootElem = ReactDOM.createRoot(document.getElementById("root"));
-rootElem.render(parentElement);
+rootElem.render(JSXElement);
