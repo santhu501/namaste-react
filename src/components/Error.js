2721df788e3e3a0ff3563a38d51dcfdc98bf3a0b
santhu501 <santhosh.mittapally@yahoo.com>
2023-06-11 10:36:06 -0400

    useRouteError hook for better error message text.

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,6 +1,13 @@
+import { useRouteError } from "react-router-dom"
+
 const Error = () => {
+ const err = useRouteError();
+
     return (
+        <div>
         <h1>Page not found for the URL you have mentioned.</h1>
+        <h2>{err.status} : {err.statusText}</h2>
+        </div>
     );
 }
 
