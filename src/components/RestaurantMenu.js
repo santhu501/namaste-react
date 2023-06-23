b6edd312b190401793d8ee4d9305260a4ff4d41e
santhu501 <santhosh.mittapally@yahoo.com>
2023-06-23 09:17:39 -0400

    show list of restaurants and menu based on restaurant id.

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,12 +1,46 @@
+import {useState, useEffect} from 'react';
+import Shimmer from './Shimmer';
+import {useParams} from 'react-router-dom';
+import { RESTAURANT_MENU_URL } from '../utils/constants';
+
 const RestaurantMenu = () => {
+
+    const [resInfo, setResInfo] = useState(null);
+    const {resId} = useParams();
+
+    useEffect(()=>{
+        fetchMenu();
+    }, []);
+
+
+    const fetchMenu = async () => {
+        const data = await fetch(RESTAURANT_MENU_URL + resId);
+        const json = await data.json();        
+        setResInfo(json?.data);
+    };
+
+    if( resInfo === null )
+    {
+        return <Shimmer/>
+    }
+
+    const { name, cuisines, costForTwoMessage } = resInfo?.cards[0]?.card?.card?.info;
+
+    const {itemCards} = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+    console.log(itemCards);
+
     return (
         <div className="menu">
-            <h1>Name of the restaurant</h1>
-            <h2>Menu</h2>
+            <h1>{name}</h1>
+            <h2>{cuisines.join(", ")} - {costForTwoMessage}</h2>
+            <h2>MENU</h2>
              <ul>
-                <li>Biryani</li>
-                <li>Tea</li>
-                <li>Juice</li>
+                {itemCards.map(item => 
+                    <li key={item?.card?.info?.id}>
+                        {item?.card?.info?.name} - {"Rs."}
+                        {item?.card?.info?.price/100 || item?.card?.info?.defaultPrice/100}
+                    </li>
+                    )}                
              </ul>
 
         </div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
 export const HEADER_LOGO_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaA7cCWhRNuAOOzw0YRD2O_xi9CCMOQyKYUfuJbObF7uesA_aaQYq5IjhaN73aeav9WYw&usqp=CAU";
 
+export const RESTAURANT_MENU_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.385044&lng=78.486671&restaurantId=";
+
 // There are two types of exports. one is default export and another is named export like above export statement.
