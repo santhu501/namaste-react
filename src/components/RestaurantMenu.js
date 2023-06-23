c5720510ffec704204e7d702df1a854f25655766
santhu501 <santhosh.mittapally@yahoo.com>
2023-06-22 20:05:37 -0400

    res url

---
+++ src/components/RestaurantMenu.js
@@ -0,0 +1,16 @@
+const RestaurantMenu = () => {
+    return (
+        <div className="menu">
+            <h1>Name of the restaurant</h1>
+            <h2>Menu</h2>
+             <ul>
+                <li>Biryani</li>
+                <li>Tea</li>
+                <li>Juice</li>
+             </ul>
+
+        </div>
+    );
+}
+
+export default RestaurantMenu;
