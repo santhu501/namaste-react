edc04e1c6053b58e37d60d512d934d61a14bf54e
santhu501 <santhosh.mittapally@yahoo.com>
2023-07-03 21:48:56 -0400

    class vs functional using state and props.

--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,17 @@
+import {useState} from 'react';
+
 const User = (props) => {
+
+    const [count] = useState(0);
+    const [count2] = useState(2);
+
     return (
         <div className="user-card">
             <h2>Name: {props.name}</h2>
             <h3>Location: Michigan </h3>
             <h4>Contact: santhosh501</h4>
+            <h4>Count: {count}</h4>
+            <h4>Count2: {count2}</h4>
         </div>
     );
 }
--- src/components/UserClassComp.js
+++ src/components/UserClassComp.js
@@ -6,14 +6,26 @@ class UserClassComp extends React.Component {
 
     constructor(props){
         super(props);
+
+        this.state = {
+            count: 0,
+            count2: 2
+        };
     }
+    
 
     render(){
+
+        const {name} = this.props;
+        const {count, count2} = this.state;
+
         return (
             <div className="user-card">
-                <h2>Name: {this.props.name}</h2>
+                <h2>Name: {name}</h2>
                 <h3>Location: Michigan </h3>
                 <h4>Contact: santhosh501</h4>
+                <h4>Count: {count}</h4>
+                <h4>Count2: {count2}</h4>
             </div>
         );
     }
