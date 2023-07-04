47d1a54fa6552c1a4f62630e55050e957b71c565
santhu501 <santhosh.mittapally@yahoo.com>
2023-07-04 18:16:24 -0400

    api call to get github user in componentDidMount

--- src/components/About.js
+++ src/components/About.js
@@ -7,15 +7,15 @@ class About extends React.Component{
 
     constructor(props){
         super(props);
-        console.log("Parent class constructor");
+       // console.log("Parent class constructor");
     }
 
     componentDidMount(){
-        console.log("Parent class component did mount");
+       // console.log("Parent class component did mount");    
     }
 
     render(){
-        console.log("Parent class render");
+     //   console.log("Parent class render");
         return (
             <div>
                 <h1>About</h1>
--- src/components/UserClassComp.js
+++ src/components/UserClassComp.js
@@ -8,40 +8,39 @@ class UserClassComp extends React.Component {
         super(props);
 
         this.state = {
-            count: 0,
-            count2: 2
+            userDetails:{
+                name: "testUser",
+                location: "hyd"
+            }
         };
-        console.log("child class constructor");
+       // console.log("child class constructor");
     }
 
-    componentDidMount() {
-        console.log("child class componet did mount");
+    async componentDidMount() {
+      //  console.log("child class componet did mount");
+
+      const data = await fetch("https://api.github.com/users/santhu501");
+      const json = await data.json();
+      console.log(json);
+
+      this.setState({
+        userDetails:json
+      })
+
     }
 
 
     render(){
 
-        const {name} = this.props;
-        const {count, count2} = this.state;
+        const {name, location} = this.state.userDetails;        
 
-        console.log("child class render");
+       // console.log("child class render");
 
         return (
             <div className="user-card">
                 <h2>Name: {name}</h2>
-                <h3>Location: Michigan </h3>
-                <h4>Contact: santhosh501</h4>
-                <h4>Count: {count}</h4>
-                {
-                 // Never update state variables directly.
-                // if there are multiple states, when we do setState on single variable, react will update only one efficiently without touching anything else.
-                }
-                <button onClick={() => {
-                    this.setState({
-                        count: this.state.count + 1
-                    });
-                }} >CountIncrement</button>
-                <h4>Fixed Count: {count2}</h4>
+                <h3>Location: {location} </h3>
+                <h4>Contact: santhosh501</h4>                
             </div>
         );
     }
