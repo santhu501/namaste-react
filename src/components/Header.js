56585d5937b10a33bc4064f56d151a54953cec89
santhu501 <santhosh.mittapally@yahoo.com>
2023-07-07 17:19:45 -0400

    online status green or red unicode characters

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import { HEADER_LOGO_URL } from "../utils/constants";
 import { useState } from "react";
 import {Link} from 'react-router-dom';
+import useOnlineStatus from "./useOnlineStatus";
 
 const Header = () => {
   const [ btnName, setBtnName ] = useState("Login");
+  const onlineStatus = useOnlineStatus();
     return (
       <div className="header">
         <div className="logo-container">
@@ -15,6 +17,9 @@ const Header = () => {
         </div>
         <div className="nav-items">
           <ul>
+            <li>
+              Online Status: {onlineStatus? "🟢":"🟥" }
+            </li>
               <li>
               <Link to="/">Home</Link> 
               </li>
