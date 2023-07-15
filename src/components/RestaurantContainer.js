625fe23349f5b108d6f521e3ad64778bd6c0a587
santhu501 <santhosh.mittapally@yahoo.com>
2023-07-15 14:08:10 -0400

    hover color

--- src/components/RestaurantContainer.js
+++ src/components/RestaurantContainer.js
@@ -1,14 +1,7 @@
-const styleInline = {
-    backgroundColor: "orange"
-  };
-  
-  // We can write directly like below for style.
-  // <div className="restaurantCard" style={{ backgroundColor: "orange"}}>
-  
   const RestaurantContainer = (props) => {
     const { resName, cuisine, rating, logo, deliveryTime } = props; // object destructuring.
     return (
-      <div className="m-6 p-4 w-[250px] h-[320px] rounded-lg" style={styleInline}>
+      <div className="m-6 p-4 w-[250px] h-[320px] rounded-lg bg-orange-200 hover:bg-orange-400">
         <img className="rounded-lg" alt={resName} src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" + logo}></img>
         <h3 className="font-bold py-2 text-base"> {resName}</h3>
         <h4> {cuisine}</h4>
