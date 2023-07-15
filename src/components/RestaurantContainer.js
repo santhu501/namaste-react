1ca2b1fe4c4581ef130a5706b76a1669c916ec08
santhu501 <santhosh.mittapally@yahoo.com>
2023-07-15 13:58:38 -0400

    Fix height of restarurant cards.

--- src/components/RestaurantContainer.js
+++ src/components/RestaurantContainer.js
@@ -8,9 +8,9 @@ const styleInline = {
   const RestaurantContainer = (props) => {
     const { resName, cuisine, rating, logo, deliveryTime } = props; // object destructuring.
     return (
-      <div className="m-4 p-4 w-[250px] rounded-lg" style={styleInline}>
+      <div className="m-6 p-4 w-[250px] h-[320px] rounded-lg" style={styleInline}>
         <img className="rounded-lg" alt={resName} src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" + logo}></img>
-        <h3 className="font-bold py-2 text-lg"> {resName}</h3>
+        <h3 className="font-bold py-2 text-base"> {resName}</h3>
         <h4> {cuisine}</h4>
         <h4> {rating} stars </h4>
         <h4> {deliveryTime} Minutes </h4>
