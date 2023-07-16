f2d2fa00847be01660e0a9d04c055b3b94270b85
santhu501 <santhosh.mittapally@yahoo.com>
2023-07-16 10:12:59 -0400

    add css to promoted.

--- src/components/RestaurantContainer.js
+++ src/components/RestaurantContainer.js
@@ -1,7 +1,7 @@
   const RestaurantContainer = (props) => {
     const { resName, cuisine, rating, logo, deliveryTime } = props; // object destructuring.
     return (
-      <div className="m-6 p-4 w-[250px] h-[320px] rounded-lg bg-orange-200 hover:bg-orange-400">
+      <div className="m-6 p-3 w-[250px] h-[310px] rounded-lg bg-orange-200 hover:bg-orange-400">
         <img className="rounded-lg" alt={resName} src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" + logo}></img>
         <h3 className="font-bold py-2 text-base"> {resName}</h3>
         <h4> {cuisine}</h4>
@@ -17,7 +17,7 @@
     return (props) => {
       return (
         <div>
-          <label>Promoted</label>
+          <label className="absolute bg-black text-white m-1 p-1 rounded-lg">Promoted</label>
           <RestaurantContainer {...props}/>
         </div>
       );
