2c81adf913273431536e44cd66461ff861830166
santhu501 <santhosh.mittapally@yahoo.com>
2023-09-10 09:13:21 -0400

    hide and show items

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,16 +1,21 @@
 import ItemList from "./ItemList";
+import { useState } from "react";
 
 const RestaurantCategory = ({data}) => {
+    const [showItems, setShowItems] = useState(false);
+    const handleClick = () => {
+        setShowItems(!showItems);
+    };
     return (
         <div>
             {/** Header */}
 
             <div className="w-6/12 mx-auto my-4 bg-gray-50">
-                <div className="flex justify-between">
+                <div className="flex justify-between" onClick={handleClick}>
                 <span className="font-bold text-lg">{data.title} ({data.itemCards.length})</span>
                 <span>⬇️</span>
                 </div>
-                <ItemList items={data.itemCards}/>
+                {showItems && <ItemList items={data.itemCards}/>}
             </div>                        
         </div>
     );
