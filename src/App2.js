32233cdf281b95f43219dcc0f35d26b8ccff3131
santhu501 <santhosh.mittapally@yahoo.com>
2023-09-27 08:24:13 -0400

    useContext usage.

--- src/App2.js
+++ src/App2.js
@@ -1,4 +1,4 @@
-import React, {lazy, Suspense} from 'react';
+import React, {lazy, Suspense, useState, useEffect, useContext} from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
 import Body from './components/Body';
@@ -7,16 +7,29 @@ import Contact from './components/Contact';
 import Error from './components/Error';
 import RestaurantMenu from './components/RestaurantMenu';
 import {createBrowserRouter, RouterProvider, Outlet} from 'react-router-dom';
+import UserContext from './utils/UserContext';
 //import Grocery from './components/Grocery';
 
 const Grocery = lazy(() => import("/src/components/Grocery"));
 
 const AppLayout = () => {
+
+    const [userName, setUserName] = useState();
+
+    useEffect(() => {
+        const data = {
+            name: "santhosh"
+        };
+        setUserName(data.name);
+    },[]);
+
     return (
+        <UserContext.Provider value={{loggedInUser: userName}}>
         <div className="app">
         <Header/>
         <Outlet/>
         </div>
+        </UserContext.Provider>
     );
 }
 
