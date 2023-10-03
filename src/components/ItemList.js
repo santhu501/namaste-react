3f64a8eeb39d35bc0b802856e53c52b351fcb7e1
santhu501 <santhosh.mittapally@yahoo.com>
2023-10-03 08:21:58 -0400

    Fix add button styling.

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -3,19 +3,19 @@ const ItemList = ({items}) =>{
         <div>
             {
                 items.map((item) => (
-                    <div key={item?.card?.info?.id} className="p-2 m-2 border-gray-200 border-b-2 text-left">                        
+                    <div key={item?.card?.info?.id} className="p-2 m-4 border-gray-300 border-b-2 text-left">                        
                         <div className="p-2">
                         <span>{item?.card?.info?.name}</span>
-                        <span> - ₹{item?.card?.info?.price ? item?.card?.info?.price/100 : item?.card?.info?.defaultPrice/100}</span>
-                        </div>
+                        <span> - ₹{item?.card?.info?.price ? item?.card?.info?.price/100 : item?.card?.info?.defaultPrice/100}</span>                        
+                        </div>                        
                         <p className="text-xs">{item?.card?.info?.description}</p>
-                        <div className="w-3/12 p-4">
+                        <div className="w-3/12 p-5">
                            <div className="absolute">
-                            <button className="p-2 mx-6 rounded-lg bg-black text-white shadow-lg">
+                            <button className="p-1 mx-3 rounded-lg bg-black text-white shadow-lg">
                                 Add +
                             </button>
                            </div>
-                        </div>
+                        </div>                        
                     </div>  
                 ))                    
            }
